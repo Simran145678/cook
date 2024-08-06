abc528f7c1b521dd0bb32d3b09eb3b371c00b613
Simran145678 <harsimrankaur708@gmail.com>
2024-08-06 12:54:13 -0400

    disclaimer modal added

---
+++ src/components/Modal.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Modal({ isVisible, onClose }) {
+  if (!isVisible) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-filter backdrop-blur-sm z-50">
+      <div className="bg-white rounded-lg shadow-lg p-6 max-w-sm mx-auto relative">
+        <button
+          className="absolute top-4 right-4 text-2xl text-gray-500 hover:text-orange-500"
+          onClick={onClose}
+        >
+          &times;
+        </button>
+        <h2 className="text-xl font-bold mb-4">Disclaimer</h2>
+        <p>
+          This website showcase only my development skills. The UX design is
+          made by Kaushal Bodar. You can browse his work by clicking on the link
+          below.
+          <span>
+            <a
+              className="block text-orange-500 hover:underline"
+              href="https://dribbble.com/kaushal_ui"
+              target="_blank"
+            >
+              https://dribbble.com/kaushal_ui
+            </a>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import Header from "../components/Header";
 import HeroBanner from "../components/HeroBanner";
 import Chef from "../assets/images/chefPro.jpg";
 import Plate from "../assets/images/platePro.jpg";
 import Card from "../components/Card";
 import Blogs from "./Blogs";
-import Footer from "../components/Footer";
+import Modal from "../components/Modal";
 import TileSlider from "../components/TileSlider";
 import Heading from "../components/Heading";
 import { Link } from "react-router-dom";
 
 export default function Home() {
   const [recipes, setRecipes] = React.useState([]);
+  const [isModalVisible, setIsModalVisible] = React.useState(true);
+
+  React.useEffect(() => {
+    setIsModalVisible(true);
+  }, []);
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+  };
 
   React.useEffect(() => {
     fetch("/api/recipes")
@@ -49,103 +57,106 @@ export default function Home() {
     },
   ];
   return (
-    <div>
-      <HeroBanner
-        para="From quick and easy meals to gourmet delights, we have something for
+    <>
+      <Modal isVisible={isModalVisible} onClose={handleCloseModal} />
+      <div>
+        <HeroBanner
+          para="From quick and easy meals to gourmet delights, we have something for
             every taste and occasion."
-        link={
-          <Link to="/recipes" className="py-2 px-2">
-            Explore all Recipies
-          </Link>
-        }
-        img={Chef}
-        varient="rounded-full"
-      >
-        <h1 className="text-2xl md:text-4xl lg:text-6xl mb-6">
-          Cook Like a Pro with our{" "}
-          <span className="text-orange-500">Easy </span> and{""}
-          <span className="text-orange-500"> Tasty</span> Recipies.
-        </h1>
-      </HeroBanner>
-      <div className="py-10 bg-yellow-200">
-        <Heading
-          heading="Popular recipes you can't miss"
-          subHeading=" From comfort food classic to exotic flavours, our featured recipies are sure to impress."
-        />
+          link={
+            <Link to="/recipes" className="py-2 px-2">
+              Explore all Recipies
+            </Link>
+          }
+          img={Chef}
+          varient="rounded-full"
+        >
+          <h1 className="text-2xl md:text-4xl lg:text-6xl mb-6">
+            Cook Like a Pro with our{" "}
+            <span className="text-orange-500">Easy </span> and{""}
+            <span className="text-orange-500"> Tasty</span> Recipies.
+          </h1>
+        </HeroBanner>
+        <div className="py-10 bg-yellow-200">
+          <Heading
+            heading="Popular recipes you can't miss"
+            subHeading=" From comfort food classic to exotic flavours, our featured recipies are sure to impress."
+          />
 
-        <div className="container w-full h-full grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-y-4 px-6 md:px-20 place-items-center">
-          {homePageRecipies.map((recipe) => {
-            return (
-              <Card
-                key={recipe.id}
-                image={recipe.imageUrl}
-                name={recipe.name}
-                description={recipe.description}
-                link={
-                  <Link to={`/recipes/${recipe.id}`} className="py-2 px-12">
-                    See Full Details
-                  </Link>
-                }
-              />
-            );
-          })}
-        </div>
-      </div>
-      <div className="mt-6">
-        <Heading
-          heading="Explore by Cuizine Type"
-          subHeading="Discover new flavours and cooking techniques with our diverse selection of cuizine types."
-        />
-        <TileSlider tiles={tiles} />
-      </div>
-      <HeroBanner
-        para="Discover new flavours and cooking techniques with our diverse selection of cuizine types."
-        link={
-          <Link to="contest" className="py-2 px-2">
-            Explore Contest
-          </Link>
-        }
-        img={Plate}
-      >
-        <h1 className="text-2xl md:text-4xl mb-6">Join Our Latest Contest</h1>
-      </HeroBanner>
-      <div className="py-10 bg-yellow-200">
-        <Heading
-          heading="Recipe Collection"
-          subHeading=" From quick weeknight dinners to eleborate weekend projects, we have recipies to fit every lifestyle and taste."
-        />
-
-        <div className="container w-full h-full grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-y-4 px-6 md:px-20 place-items-center">
-          {homePageCards.map((recipe) => {
-            return (
-              <Link to={`/recipes/${recipe.id}`}>
+          <div className="container w-full h-full grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-y-4 px-6 md:px-20 place-items-center">
+            {homePageRecipies.map((recipe) => {
+              return (
                 <Card
-                  varient="hover:bg-orange-500 group-hover:border-white cursor-pointer group-hover:text-white hover:text-white transition-all duration-300 ease-in-out"
                   key={recipe.id}
                   image={recipe.imageUrl}
                   name={recipe.name}
                   description={recipe.description}
-                  duration={recipe.duration}
-                  level={recipe.level}
-                  rating={recipe.rating}
+                  link={
+                    <Link to={`/recipes/${recipe.id}`} className="py-2 px-12">
+                      See Full Details
+                    </Link>
+                  }
                 />
-              </Link>
-            );
-          })}
+              );
+            })}
+          </div>
+        </div>
+        <div className="mt-6">
+          <Heading
+            heading="Explore by Cuizine Type"
+            subHeading="Discover new flavours and cooking techniques with our diverse selection of cuizine types."
+          />
+          <TileSlider tiles={tiles} />
+        </div>
+        <HeroBanner
+          para="Discover new flavours and cooking techniques with our diverse selection of cuizine types."
+          link={
+            <Link to="contest" className="py-2 px-2">
+              Explore Contest
+            </Link>
+          }
+          img={Plate}
+        >
+          <h1 className="text-2xl md:text-4xl mb-6">Join Our Latest Contest</h1>
+        </HeroBanner>
+        <div className="py-10 bg-yellow-200">
+          <Heading
+            heading="Recipe Collection"
+            subHeading=" From quick weeknight dinners to eleborate weekend projects, we have recipies to fit every lifestyle and taste."
+          />
+
+          <div className="container w-full h-full grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-y-4 px-6 md:px-20 place-items-center">
+            {homePageCards.map((recipe) => {
+              return (
+                <Link to={`/recipes/${recipe.id}`}>
+                  <Card
+                    varient="hover:bg-orange-500 group-hover:border-white cursor-pointer group-hover:text-white hover:text-white transition-all duration-300 ease-in-out"
+                    key={recipe.id}
+                    image={recipe.imageUrl}
+                    name={recipe.name}
+                    description={recipe.description}
+                    duration={recipe.duration}
+                    level={recipe.level}
+                    rating={recipe.rating}
+                  />
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+        <div className="py-12">
+          <Heading
+            heading="Latest Blog"
+            subHeading=" From quick weeknight dinners to eleborate weekend projects."
+          />
+          <Blogs />
+          <Link to="/blogs">
+            <button className=" bg-orange-500 text-white text-sm mx-auto block border-xl py-2 px-3 rounded-lg hover:underline ">
+              See more
+            </button>
+          </Link>
         </div>
       </div>
-      <div className="py-12">
-        <Heading
-          heading="Latest Blog"
-          subHeading=" From quick weeknight dinners to eleborate weekend projects."
-        />
-        <Blogs />
-        <Link to="/blogs">
-          <button className=" bg-orange-500 text-white text-sm mx-auto block border-xl py-2 px-3 rounded-lg hover:underline ">
-            See more
-          </button>
-        </Link>
-      </div>
-    </div>
+    </>
   );
 }
